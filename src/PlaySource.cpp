ff8d3d8f7ccd9a40ad4a73efa16730ec5268086f
farova <michaela.farova@gmail.com>
2015-07-23 16:01:44 -0400

    Adding TEMP code for PlaySOurce to just print song to console and move on to next song

--- src/PlaySource.cpp
+++ src/PlaySource.cpp
@@ -1,11 +1,20 @@
 #include "PlaySource.h"
 
+#include <stdlib.h>
+
 void PlaySource::run() {
 
 	skrillex::ResultSet<skrillex::Song> queue;
+	m_db->getQueue(queue);
 
+	for (auto& song : queue) {
 
+		//TEMP
+		printf("%s: %s (%s)", song.artist.name, song.name, song.genre.name);
+		// END TEMP
 
-
+		// Song finished
+		m_db->songFinished();
+	}
 
 }
