01b2dbf99a71fdd6f748d94a3b8bb02f15f345d1
Mike Cheng <mfycheng@uwaterloo.ca>
2016-03-11 18:46:27 -0500

    Gut old code, add config file option, and systemd service file

--- cmd/playsource.go
+++ cmd/playsource.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
 	"net"
+	"os"
 	"time"
 
 	"github.com/crowdsoundsystem/playsource/pkg/playsource"
@@ -14,6 +16,7 @@ import (
 )
 
 var (
+	configPath   = flag.String("config", "", "Configuration path")
 	mopidyUrl    = flag.String("mopidyUrl", "http://localhost:6680/mopidy/rpc", "Mopidy RPC endpoint")
 	port         = flag.Int("port", 50052, "Port to listen on")
 	queueSize    = flag.Int("queueSize", 3, "Anticipated client queue size")
@@ -21,10 +24,38 @@ var (
 	test         = flag.Bool("test", false, "Whether or not to emulate a real server")
 )
 
+type Config struct {
+	MopidyURL    string `json:"mopidy_url"`
+	Port         int    `json:"port"`
+	QueueSize    int    `json:"queue_size"`
+	PollInterval int    `json:"poll_interval"`
+	Test         bool   `json:"test"`
+}
+
 func main() {
 	flag.Parse()
 
-	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%v", *port))
+	var config Config
+	if *configPath != "" {
+		f, err := os.Open(*configPath)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		d := json.NewDecoder(f)
+		err = d.Decode(&config)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		config.MopidyURL = *mopidyUrl
+		config.Port = *port
+		config.QueueSize = *queueSize
+		config.PollInterval = *pollInterval
+		config.Test = *test
+	}
+
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%v", config.Port))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -35,7 +66,7 @@ func main() {
 		playsource.RegisterPlaysourceServer(
 			grpcServer,
 			server.NewTestServer(
-				*queueSize,
+				config.QueueSize,
 				0.9,
 				5*time.Second,
 			),
@@ -44,13 +75,13 @@ func main() {
 		playsource.RegisterPlaysourceServer(
 			grpcServer,
 			server.NewMopidyServer(
-				*mopidyUrl,
-				*queueSize,
-				time.Duration(*pollInterval)*time.Second,
+				config.MopidyURL,
+				config.QueueSize,
+				time.Duration(config.PollInterval)*time.Second,
 			),
 		)
 	}
 
-	log.Println("Listenning on:", fmt.Sprintf("localhost:%v", *port))
+	log.Println("Listenning on:", fmt.Sprintf("localhost:%v", config.Port))
 	grpcServer.Serve(lis)
 }
